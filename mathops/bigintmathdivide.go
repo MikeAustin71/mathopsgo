a42caf99434404d22ae8eb511f409c2cfc97eaa7
Mike Rapp <mike@paladinacs.net>
2018-05-05 22:05:09 -0500

    Added tests for BigIntMathDivide.INumMgrFracQuotient() and BigIntMathDivide.INumMgrFracQuotientArray()

--- mathops/bigintmathdivide.go
+++ mathops/bigintmathdivide.go
@@ -666,7 +666,6 @@ func (bIDivide BigIntMathDivide) DecimalFracQuotientArray(
 	return fracQuoArray, nil
 }
 
-
 // IntAryQuotientMod - Performs a division operation on IntAry type input
 // parameters, 'dividend' and 'divisor'.
 //
@@ -1041,4 +1040,189 @@ func (bIDivide BigIntMathDivide) INumMgrQuotientMod(
 	err = nil
 
 	return quotient, modulo, err
+}
+
+// INumMgrFracQuotient - Performs a division operation on input
+// parameters 'dividend' and 'divisor' which implement the INumMgr
+// interface.
+//
+// The resulting quotient is returned as a BigIntNum type representing the
+// result of the division operation expressed as integer and fractional digits.
+// Remember that the BigIntNum type specifies 'precision'. Precision is defined
+// as the number of fractional digits to the right of the decimal place.
+//
+// The input parameter 'maxPrecision' is used to control the precision of the
+// resulting fractional quotient. Be advised that this method is capable of
+// calculating quotients with very long strings of fractional digits. Therefore,
+// the user is advised to set a relevant value for 'maxPrecision'.
+//
+// Examples:
+// =========
+//
+// Note: For all examples maximum precision is specified as '15'.
+// ----------------------------------------------------------------------------
+//																				     Quotient
+//  Dividend		divided by	Divisor		=		  BigIntNum Integer 	Precision	 Result
+//  -------- 	  ----------	--------				-----------------	  ---------	 ------
+// 	 10.5  				/ 				2 				= 			525  							  2  			 5.25
+// 	 10    				/ 				2 				= 			5	  							  0  			 5
+//   11.5  				/         2.5				=  			46								  1				 4.6
+//    2.5					/				 12.555			=				199123855037834	   15				 0.199123855037834
+//	-12.555 			/ 				2.5 			= 		 -5022							  3				-5.022
+//  -12.555     	/    			2  			  = 		 -62775							  4				-6.2775
+//  - 2.5 				/ 			 12.555		  = 		 -199123855037834	   15				-0.199123855037834
+// 	 12.555				/ 			- 2.5			  =			 -5022								3				-5.022
+//   12.555 			/ 			- 2 				= 		 -62775								4				-6.2775
+//    2.5 				/ 			-12.555		  = 		 -199123855037834	   15				-0.199123855037834
+// 	-12.555 			/ 			- 2.5 			= 			5022								3				 5.022
+//  -12.555     	/    		- 2 				= 		  62775								4				 6.2775
+//  - 2.5	 				/ 			-12.555		  = 		  199123855037834	   15				 0.199123855037834
+//  -10						/				- 2					=				5														 5
+//
+func (bIDivide BigIntMathDivide) INumMgrFracQuotient(
+														dividend,
+															divisor INumMgr,
+																maxPrecision uint) (fracQuotient BigIntNum, err error) {
+
+	ePrefix := "BigIntMathDivide.INumMgrFracQuotient() "
+
+	if divisor.IsZero() {
+		fracQuotient = BigIntNum{}.New()
+		err = errors.New(ePrefix + "Error: Attempted divide by zero!")
+		return fracQuotient, err
+	}
+
+	bPair, errx := BigIntPair{}.NewINumMgr(dividend, divisor)
+
+	if errx != nil {
+		fracQuotient = BigIntNum{}.New()
+		err = fmt.Errorf(ePrefix +
+			"Error returned by BigIntPair{}.NewINumMgr(dividend, divisor). " +
+			"dividend='%v' divisor='%v' Error='%v'",
+			dividend.GetNumStr(), divisor.GetNumStr(), err.Error())
+
+		return fracQuotient, err
+	}
+
+	bPair.MakePrecisionsEqual()
+
+	rDividend := big.NewRat(1, 1).SetInt(bPair.Big1.bigInt)
+	rDivisor := big.NewRat(1, 1).SetInt(bPair.Big2.bigInt)
+	rQuotient := big.NewRat(1, 1).Quo(rDividend, rDivisor)
+	numStr := rQuotient.FloatString(int(maxPrecision))
+
+	fracQuotient, errx =
+		BigIntNum{}.NewNumStr(numStr)
+
+	if errx != nil {
+		fracQuotient = BigIntNum{}.New()
+		err =	fmt.Errorf(ePrefix +
+			"Error returned by BigIntNum{}.NewNumStr(numStr, maxPrecision). " +
+			"numStr='%v' maxPrecision='%v' Error='%v'",
+			numStr, maxPrecision, err.Error())
+
+		return fracQuotient, err
+	}
+
+	fracQuotient.TrimTrailingFracZeros()
+
+	return fracQuotient, nil
+}
+
+// INumMgrFracQuotientArray - Performs a division operation on input parameters
+// 'dividends' and 'divisor' which implement the INumMgr interfce. 'dividends'
+// is an array of types implementing the INumMgr interface. The division operation
+// is performed on each element of the 'dividends' array using a single 'divisor'.
+//
+// The resulting quotients are returned as an array of types implementing the INumMgr
+// Interface. The values represent the results of each division operation expressed
+// as integer and fractional digits.
+//
+// The input parameter 'maxPrecision' is used to control the precision of the
+// resulting fractional quotient. Precision is defined as the number of numeric
+// digits to the right of the decimal point. Be advised that this method is
+// capable of calculating quotients with very long strings of fractional digits.
+// Therefore the user is advised to set a relevant value for 'maxPrecision'.
+//
+// Examples:
+// =========
+// Note: For all examples maximum precision is specified as '15'.
+// ----------------------------------------------------------------------------
+//    	                                       Returned
+//  	Dividend		divided by	Divisor		=	       Array						=  	 Result
+//  	-------- 	  ----------	--------				-----------------	   		---------
+//	 	 10.5  				 / 				2.5 			= 		 fracQuoArray[0]		=   	 4.2
+//	 	 10    				 / 				2.5 			= 		 fracQuoArray[1]		=	 	   4
+//	   11.5  				 /        2.5				=  		 fracQuoArray[2]		=		   4.6
+//	    2.5					 /				2.5			  =			 fracQuoArray[3]    =      1
+//		-12.555 			 / 				2.5 			= 		 fracQuoArray[4]    =  -   5.022
+//	  - 2.5 				 / 			  2.5		    = 		 fracQuoArray[5]    =  -   1
+//	   12.555 			 / 			  2.5 			= 		 fracQuoArray[6]    =      5.022
+//	 -122.783 			 / 			  2.5 			= 		 fracQuoArray[7]    =  -  49.1132
+//	-6847.231   	   /    	  2.5 			= 		 fracQuoArray[8]    =  -2738.8924
+//	  - 2.5	 				 / 			  2.5		    = 		 fracQuoArray[9]    =  -   1
+//	  -10						 /			  2.5				=			 fracQuoArray[10]   =  -   4
+//	  -10.5					 /			  2.5				=			 fracQuoArray[11]   =  -   4.2
+//
+func (bIDivide BigIntMathDivide) INumMgrFracQuotientArray(
+												dividends []INumMgr,
+													divisor INumMgr,
+														maxPrecision uint) (fracQuoArray [] INumMgr, err error) {
+
+	ePrefix := "BigIntMathDivide.INumMgrFracQuotientArray() "
+
+	if divisor.IsZero() {
+		fracQuoArray = []INumMgr{}
+		err = errors.New(ePrefix + "Error: Attempted divide by zero!")
+		return fracQuoArray, err
+	}
+
+	lenAry := len(dividends)
+
+	if lenAry == 0 {
+		fracQuoArray = []INumMgr{}
+		err = errors.New(ePrefix + "Error: Input Parameter 'dividends' is an EMPTY Array!")
+		return fracQuoArray, err
+	}
+
+	fracQuoArray = make([]INumMgr, lenAry, lenAry+20)
+
+	for i:=0; i < lenAry; i++ {
+
+		bPair, errx := BigIntPair{}.NewINumMgr(dividends[i], divisor)
+
+		if errx != nil {
+			fracQuoArray = []INumMgr{}
+			err =	fmt.Errorf(ePrefix +
+				"Error returned by BigIntPair{}.NewINumMgr(dividends[i], divisor). " +
+				"dividends[%v]='%v' divisor='%v' Error='%v'",
+				i, dividends[i].GetNumStr(), divisor.GetNumStr(), errx.Error())
+			return fracQuoArray, err
+		}
+
+		bPair.MakePrecisionsEqual()
+
+		rDividend := big.NewRat(1, 1).SetInt(bPair.Big1.bigInt)
+		rDivisor := big.NewRat(1, 1).SetInt(bPair.Big2.bigInt)
+		rQuotient := big.NewRat(1, 1).Quo(rDividend, rDivisor)
+		numStr := rQuotient.FloatString(int(maxPrecision))
+
+		bINum, errx := BigIntNum{}.NewNumStr(numStr)
+
+		if errx != nil {
+			fracQuoArray = []INumMgr{}
+			err =	fmt.Errorf(ePrefix +
+				"Error returned by BigIntNum{}.NewNumStr(numStr). " +
+				"numStr='%v' maxPrecision='%v' Error='%v'",
+				numStr, maxPrecision, errx.Error())
+			return fracQuoArray, err
+		}
+
+		bINum.TrimTrailingFracZeros()
+
+		fracQuoArray[i] = &bINum
+
+	}
+
+	return fracQuoArray, nil
 }
--- mathops/xt_bigintmathdivide_01_test.go
+++ mathops/xt_bigintmathdivide_01_test.go
@@ -4659,3 +4659,295 @@ func TestBigIntMathDivide_INumMgrQuotientMod_04(t *testing.T) {
 	}
 
 }
+
+func TestBigIntMathDivide_INumMgrFracQuotient_01(t *testing.T) {
+	// Dividend		divided by		Divisor			=		Quotient
+	// 	 10.5  				/ 					2 				= 	 5.25
+
+	dividendStr 			:= "10.5"
+	divisorStr  			:= "2"
+	expectedQuoStr 		:= "5.25"
+	maxPrecision			:= uint(15)
+
+	dividend, err := Decimal{}.NewNumStr(dividendStr)
+
+	if err != nil {
+		t.Errorf("Error returned by Decimal{}.NewNumStr(dividendStr). " +
+			"dividendStr='%v' Error='%v' ",
+			dividendStr, err.Error())
+	}
+
+	divisor, err := NumStrDto{}.NewNumStr(divisorStr)
+
+	if err != nil {
+		t.Errorf("Error returned by NumStrDto{}.NewNumStr(divisorStr). " +
+			"divisorStr='%v' Error='%v' ",
+			divisorStr, err.Error())
+	}
+
+	expectedQuo, err := BigIntNum{}.NewNumStr(expectedQuoStr)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntNum{}.NewNumStr(expectedQuoStr). " +
+			"expectedQuoStr='%v' Error='%v' ",
+			expectedQuoStr, err.Error())
+	}
+
+	quotient, err :=
+		BigIntMathDivide{}.INumMgrFracQuotient(&dividend, &divisor, maxPrecision)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntMathDivide{}.INumMgrFracQuotient" +
+			"(dividend, divisor, maxPrecision).  Error='%v' ",
+			err.Error())
+	}
+
+	if !expectedQuo.Equal(quotient) {
+		t.Errorf("Error: Expected Quotient='%v'. Instead Quotient='%v'",
+			expectedQuo.GetNumStr(), quotient.GetNumStr())
+	}
+}
+
+func TestBigIntMathDivide_INumMgrFracQuotient_02(t *testing.T) {
+	// Dividend		divided by		Divisor			=		Quotient
+	//	-12.555 			/ 					2.5 			= 		-5.022
+
+	dividendStr 			:= "-12.555"
+	divisorStr  			:= "2.5"
+	expectedQuoStr 		:= "-5.022"
+	maxPrecision			:= uint(15)
+
+	dividend, err := NumStrDto{}.NewNumStr(dividendStr)
+
+	if err != nil {
+		t.Errorf("Error returned by NumStrDto{}.NewNumStr(dividendStr). " +
+			"dividendStr='%v' Error='%v' ",
+			dividendStr, err.Error())
+	}
+
+	divisor, err := IntAry{}.NewNumStr(divisorStr)
+
+	if err != nil {
+		t.Errorf("Error returned by IntAry{}.NewNumStr(divisorStr). " +
+			"divisorStr='%v' Error='%v' ",
+			divisorStr, err.Error())
+	}
+
+	expectedQuo, err := BigIntNum{}.NewNumStr(expectedQuoStr)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntNum{}.NewNumStr(expectedQuoStr). " +
+			"expectedQuoStr='%v' Error='%v' ",
+			expectedQuoStr, err.Error())
+	}
+
+	quotient, err :=
+		BigIntMathDivide{}.INumMgrFracQuotient(&dividend, &divisor, maxPrecision)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntMathDivide{}.INumMgrFracQuotient" +
+			"(dividend, divisor, maxPrecision).  Error='%v' ",
+			err.Error())
+	}
+
+	if !expectedQuo.Equal(quotient) {
+		t.Errorf("Error: Expected Quotient='%v'. Instead Quotient='%v'",
+			expectedQuo.GetNumStr(), quotient.GetNumStr())
+	}
+}
+
+func TestBigIntMathDivide_INumMgrFracQuotient_03(t *testing.T) {
+	// Dividend		divided by		Divisor			=		Quotient
+	//  - 2.5 				/ 			 	12.555		  = 	-0.199123855037834
+
+	dividendStr 			:= "-2.5"
+	divisorStr  			:= "12.555"
+	expectedQuoStr 		:= "-0.199123855037834"
+	maxPrecision			:= uint(15)
+
+	dividend, err := BigIntNum{}.NewNumStr(dividendStr)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntNum{}.NewNumStr(dividendStr). " +
+			"dividendStr='%v' Error='%v' ",
+			dividendStr, err.Error())
+	}
+
+	divisor, err := Decimal{}.NewNumStr(divisorStr)
+
+	if err != nil {
+		t.Errorf("Error returned by Decimal{}.NewNumStr(divisorStr). " +
+			"divisorStr='%v' Error='%v' ",
+			divisorStr, err.Error())
+	}
+
+	expectedQuo, err := BigIntNum{}.NewNumStr(expectedQuoStr)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntNum{}.NewNumStr(expectedQuoStr). " +
+			"expectedQuoStr='%v' Error='%v' ",
+			expectedQuoStr, err.Error())
+	}
+
+	quotient, err :=
+		BigIntMathDivide{}.INumMgrFracQuotient(&dividend, &divisor, maxPrecision)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntMathDivide{}.INumMgrFracQuotient" +
+			"(dividend, divisor, maxPrecision).  Error='%v' ",
+			err.Error())
+	}
+
+	if !expectedQuo.Equal(quotient) {
+		t.Errorf("Error: Expected Quotient='%v'. Instead Quotient='%v'",
+			expectedQuo.GetNumStr(), quotient.GetNumStr())
+	}
+}
+
+func TestBigIntMathDivide_INumMgrFracQuotient_04(t *testing.T) {
+	// Dividend		divided by		Divisor			=		Quotient
+	// 	-12.555 			/ 				 -2.5 			= 	 5.022
+
+	dividendStr 			:= "-12.555"
+	divisorStr  			:= "-2.5"
+	expectedQuoStr 		:= "5.022"
+	maxPrecision			:= uint(15)
+
+	dividend, err := IntAry{}.NewNumStr(dividendStr)
+
+	if err != nil {
+		t.Errorf("Error returned by IntAry{}.NewNumStr(dividendStr). " +
+			"dividendStr='%v' Error='%v' ",
+			dividendStr, err.Error())
+	}
+
+	divisor, err := BigIntNum{}.NewNumStr(divisorStr)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntNum{}.NewNumStr(divisorStr). " +
+			"divisorStr='%v' Error='%v' ",
+			divisorStr, err.Error())
+	}
+
+	expectedQuo, err := BigIntNum{}.NewNumStr(expectedQuoStr)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntNum{}.NewNumStr(expectedQuoStr). " +
+			"expectedQuoStr='%v' Error='%v' ",
+			expectedQuoStr, err.Error())
+	}
+
+	quotient, err :=
+		BigIntMathDivide{}.INumMgrFracQuotient(&dividend, &divisor, maxPrecision)
+
+	if err != nil {
+		t.Errorf("Error returned by BigIntMathDivide{}.INumMgrFracQuotient" +
+			"(dividend, divisor, maxPrecision).  Error='%v' ",
+			err.Error())
+	}
+
+	if !expectedQuo.Equal(quotient) {
+		t.Errorf("Error: Expected Quotient='%v'. Instead Quotient='%v'",
+			expectedQuo.GetNumStr(), quotient.GetNumStr())
+	}
+}
+
+
+func TestBigIntMathDivide_INumMgrFracQuotientArray_01(t *testing.T) {
+
+	divisorStr := "2.5"
+	maxPrecision := uint(15)
+
+	dividendArrayStr := [] string {
+		"10.5",
+		"10",
+		"11.5",
+		"2.5",
+		"-12.555",
+		"-2.5",
+		"12.555",
+		"-122.783",
+		"-6847.231",
+		"-2.5",
+		"-10",
+		"-10.5",
+	}
+
+	expectedArrayStr := [] string {
+		"4.2",
+		"4",
+		"4.6",
+		"1",
+		"-5.022",
+		"-1",
+		"5.022",
+		"-49.1132",
+		"-2738.8924",
+		"-1",
+		"-4",
+		"-4.2",
+	}
+
+	lenDividends := len(dividendArrayStr)
+
+	divisor, err := IntAry{}.NewNumStr(divisorStr)
+
+	if err != nil {
+		t.Errorf("Error returned by Decimal{}.NewNumStr(divisorStr). " +
+			"divisor='%v' Error='%v' ",
+			divisorStr, err.Error())
+	}
+
+	dividends := make([]INumMgr, lenDividends)
+	expectedResults := make([]BigIntNum, lenDividends)
+
+	for i:=0; i < lenDividends; i++ {
+
+		dec, err := Decimal{}.NewNumStr(dividendArrayStr[i])
+
+		if err != nil {
+			t.Errorf("Error returned by Decimal{}.NewNumStr(dividendArrayStr[i]). " +
+				"dividendArrayStr[%v]='%v' Error='%v' ",
+				i, dividendArrayStr[i], err.Error())
+		}
+
+		dividends[i] = &dec
+
+		expectedResults[i], err = BigIntNum{}.NewNumStr(expectedArrayStr[i])
+
+		if err != nil {
+			t.Errorf("Error returned by BigIntNum{}.NewNumStr(expectedArrayStr[i]). " +
+				"expectedArrayStr[%v]='%v' Error='%v' ",
+				i, expectedArrayStr[i], err.Error())
+		}
+
+	}
+
+	resultArray, err := BigIntMathDivide{}.INumMgrFracQuotientArray(dividends, &divisor, maxPrecision )
+
+	if err != nil {
+		t.Errorf("Error returned by INumMgrFracQuotientArray{}.INumMgrFracQuotientArray"+
+			"(dividends, divisor, maxPrecision ). " +
+			"divisor='%v' maxPrecision='%v' Error='%v' ",
+			divisor.GetNumStr(), maxPrecision, err.Error())
+	}
+
+	lenResultArray := len(resultArray)
+
+	if lenDividends != lenResultArray {
+		t.Errorf("Error: Expected Results Array Length='%v'. Actual Array Length='%v'.",
+			lenDividends, lenResultArray)
+	}
+
+	for k:=0; k < lenDividends; k++ {
+
+		expectedNumStr := expectedResults[k].GetNumStr()
+		actualNumStr := resultArray[k].GetNumStr()
+
+		if expectedNumStr != actualNumStr {
+			t.Errorf("Error: Expected Value='%v'. Actual Value='%v' k='%v'",
+				expectedNumStr, actualNumStr, k)
+		}
+
+	}
+}
