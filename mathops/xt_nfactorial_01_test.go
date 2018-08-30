5f644f9f746eadad071a218b5580b25528028d1c
Mike Rapp <mike@paladinacs.net>
2018-08-30 12:03:20 -0500

    Added TestNFactorial_CalcFactorialValueBigInt_02.

--- mathops/xt_nfactorial_01_test.go
+++ mathops/xt_nfactorial_01_test.go
@@ -5,11 +5,35 @@ import (
 	"testing"
 )
 
-func TestNFactorial_CalcFactorialValueBigInt(t *testing.T) {
+func TestNFactorial_CalcFactorialValueBigInt_01(t *testing.T) {
 
 	n := big.NewInt(5)
 
 	lowerLimit := big.NewInt(1)
+
+	expectedResultStr := "120"
+
+	result, err := NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit)
+
+	if err != nil {
+		t.Errorf("Error returned by NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit). " +
+			"Error='%v' ", err.Error())
+	}
+
+	actualResultStr := result.GetNumStr()
+
+	if expectedResultStr != actualResultStr {
+		t.Errorf("Error: Expected result='%v'. Instead, result='%v' ",
+			expectedResultStr, actualResultStr)
+	}
+
+}
+
+func TestNFactorial_CalcFactorialValueBigInt_02(t *testing.T) {
+
+	n := big.NewInt(5)
+
+	lowerLimit := big.NewInt(0)
 
 	expectedResultStr := "120"
 
