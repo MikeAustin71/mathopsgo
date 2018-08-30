e4ed7a2fd9b109b72a6ec1a34f2a254cae68481e
Mike Rapp <mike@paladinacs.net>
2018-08-30 14:43:00 -0500

    Added tests through TestNFactorial_CalcFactorialValueBigInt_06.

--- mathops/xt_nfactorial_01_test.go
+++ mathops/xt_nfactorial_01_test.go
@@ -52,3 +52,83 @@ func TestNFactorial_CalcFactorialValueBigInt_02(t *testing.T) {
 	}
 
 }
+
+func TestNFactorial_CalcFactorialValueBigInt_03(t *testing.T) {
+
+	n := big.NewInt(-5)
+
+	lowerLimit := big.NewInt(0)
+
+	_, err := NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit)
+
+	if err == nil {
+		t.Error("Expected error return from CalcFactorialValueBigInt(n, lowerLimit) " +
+			"n=-1")
+
+	}
+
+}
+
+func TestNFactorial_CalcFactorialValueBigInt_04(t *testing.T) {
+
+	n := big.NewInt(5)
+
+	lowerLimit := big.NewInt(-2)
+
+	_, err := NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit)
+
+	if err == nil {
+		t.Error("Expected error return from CalcFactorialValueBigInt(n, lowerLimit) " +
+			"lower limit =-2")
+
+	}
+
+}
+
+func TestNFactorial_CalcFactorialValueBigInt_05(t *testing.T) {
+
+	n := big.NewInt(5)
+
+	lowerLimit := big.NewInt(2)
+
+	expectedResultStr := "60"
+
+	result, err := NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit)
+
+	if err != nil {
+		t.Errorf("Error returned by NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit). " +
+			"Error='%v' ", err.Error())
+	}
+
+	actualResultStr := result.GetNumStr()
+
+	if expectedResultStr != actualResultStr {
+		t.Errorf("Error: Expected result='%v'. Instead, result='%v' ",
+			expectedResultStr, actualResultStr)
+	}
+
+}
+
+func TestNFactorial_CalcFactorialValueBigInt_06(t *testing.T) {
+
+	n := big.NewInt(5)
+
+	lowerLimit := big.NewInt(4)
+
+	expectedResultStr := "5"
+
+	result, err := NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit)
+
+	if err != nil {
+		t.Errorf("Error returned by NFactorial{}.CalcFactorialValueBigInt(n, lowerLimit). " +
+			"Error='%v' ", err.Error())
+	}
+
+	actualResultStr := result.GetNumStr()
+
+	if expectedResultStr != actualResultStr {
+		t.Errorf("Error: Expected result='%v'. Instead, result='%v' ",
+			expectedResultStr, actualResultStr)
+	}
+
+}
