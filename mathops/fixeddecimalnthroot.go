e4144898ce2c9f2981ea6bf8cc0b75f7c019f9c0
Mike Rapp <mike@paladinacs.net>
2018-10-10 02:33:55 -0500

    End of day.

--- mathops/fixeddecimalnthroot.go
+++ mathops/fixeddecimalnthroot.go
@@ -1,6 +1,9 @@
 package mathops
 
-import "math/big"
+import (
+	"errors"
+	"math/big"
+)
 
 type FixedDecimalNthRoot struct {
 	NthRoot BigIntFixedDecimal
@@ -12,11 +15,58 @@ type FixedDecimalNthRoot struct {
 
 // GetNextIntegerBundle - Returns the next bundle for an integer value.
 func (fdNRt FixedDecimalNthRoot) GetNextIntegerBundle(
-	integer, nRoot *big.Int) ( nextBundle, integerResidual *big.Int, err error) {
+	integer,
+	integerMagnitude,
+	nthRoot *big.Int,
+	intBundleNo int) ( nextBundle,
+											integerResidual,
+											magnitudeResidual *big.Int,
+											thisBundleNo int,
+											err error) {
 
 	nextBundle = big.NewInt(0)
 	integerResidual = big.NewInt(0)
+	magnitudeResidual = big.NewInt(0)
+	thisBundleNo = 0
 	err = nil
 
-	return nextBundle, integerResidual, err
+	ePrefix := "FixedDecimalNthRoot.GetNextIntegerBundle() "
+
+	bigZero := big.NewInt(0)
+
+	if nthRoot.Cmp(bigZero) == 0 {
+		err = errors.New(ePrefix + "Error: Input Parameter nthRoot = ZERO!")
+		return nextBundle, integerResidual, magnitudeResidual, thisBundleNo, err
+	}
+
+	totalDigits := big.NewInt(0).Add(integerMagnitude, big.NewInt(1))
+	numOfBundlesInThisInteger := big.NewInt(0)
+	remainingDigits := big.NewInt(0)
+	scratch := big.NewInt(0)
+	factor := big.NewInt(0)
+
+	numOfBundlesInThisInteger, remainingDigits = big.NewInt(0).QuoRem(integerMagnitude, nthRoot, scratch)
+
+	cmpNumOfBundles := numOfBundlesInThisInteger.Cmp(bigZero)
+	cmpRemainDigits := remainingDigits.Cmp(bigZero)
+
+	if cmpNumOfBundles == 0 && cmpRemainDigits == 0 {
+		thisBundleNo = -1
+		return nextBundle, integerResidual, magnitudeResidual, thisBundleNo, err
+	}
+
+	if cmpRemainDigits == 1 {
+		factor.Set(remainingDigits)
+	} else {
+		// cmpNumOfBundles MUST BE == 1
+		factor.Set(nthRoot)
+	}
+
+	delta := totalDigits.Sub(totalDigits, factor)
+	scale := big.NewInt(0).Exp(big.NewInt(10), delta, nil)
+	nextBundle,integerResidual = big.NewInt(0).QuoRem(integer,scale, scratch)
+	magnitudeResidual = big.NewInt(0).Sub(delta, big.NewInt(1))
+	thisBundleNo = intBundleNo + 1
+	err = nil
+	return nextBundle, integerResidual, magnitudeResidual, thisBundleNo, err
 }
