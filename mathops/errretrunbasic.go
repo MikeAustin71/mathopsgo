282c495e8e8f0ada7aeaf2dd00baca55a7ee217c
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-05-05 17:02:34 -0500

    Refactored custom error type

---
+++ mathops/errretrunbasic.go
@@ -0,0 +1,62 @@
+package mathops
+
+// ErrorReturnBasic
+// Custom error message used to specify the
+// function returning the error message
+type ErrorReturnBasic struct {
+	ErrPrefix  string
+	ReturnFunc string
+	ErrContext string
+	ErrMessage string
+}
+
+// Error
+// The following format is used to intialize
+// this type of error:
+//
+//	if err != nil {
+//
+//		return Decimal{},
+//			&ErrorReturnBasic{
+//				ErrPrefix:  ePrefix.String(),
+//				ReturnFunc: "dec, err := new(Decimal).NewBigInt(\n" +
+//				"  big.NewInt(0).Set(bNum.bigInt), bNum.precision)\n",
+//				ErrContext: "",
+//				ErrMessage:   err.Error(),
+//			}
+//	}
+//
+//	NOTE:
+//
+//	Element 'ErrContext' is optional
+func (e *ErrorReturnBasic) Error() string {
+
+	var errStr string
+	foundCnt := 0
+
+	if e.ErrPrefix != "" {
+		errStr = e.ErrPrefix + "\n"
+		foundCnt++
+	}
+
+	if e.ReturnFunc != "" {
+		errStr += "Error returned by: \\n  " + e.ReturnFunc + "\n"
+		foundCnt++
+	}
+
+	if e.ErrContext != "" {
+		errStr += e.ErrContext + "\n"
+		foundCnt++
+	}
+
+	if e.ErrMessage != "" {
+		errStr += "Error: \n  " + e.ErrMessage + "\n"
+		foundCnt++
+	}
+
+	if foundCnt == 0 {
+		errStr = "No Error parameters provided!\n"
+	}
+
+	return errStr
+}
