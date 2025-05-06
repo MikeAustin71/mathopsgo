cee533712bffd0d65e3e7e36226fff285c98efa9
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-05-06 12:20:09 -0500

    Added new methods

--- mathops/bigintnumneutron.go
+++ mathops/bigintnumneutron.go
@@ -11,16 +11,22 @@ type bigIntNumNeutron struct {
 	lock *sync.Mutex
 }
 
-// inverse
+// inverseBigIntNum
 //
-// Returns the inverse of the current BigIntNum value.
-// The inverse of the value is equal to one ('1') divided by the
+// Returns the inverseBigIntNum of the current BigIntNum value.
+// The inverseBigIntNum of the value is equal to one ('1') divided by the
 // numeric value of the current BigIntNum.
 //
 // The BigIntNum return value for this operation will contain numeric
 // separators (decimal separator, thousands separator and currency
 // symbol) copied from the original BigIntNum instance.
-func (bNumNeutron *bigIntNumNeutron) inverse(
+//
+//	NOTE:
+//
+// This method does NOT test the validity of 'bNum', an
+// instance of type BigIntNum. The calling method must
+// do this!
+func (bNumNeutron *bigIntNumNeutron) inverseBigIntNum(
 	bNum *BigIntNum,
 	maxPrecision uint,
 	errPrefDto *ePref.ErrPrefixDto) (BigIntNum, error) {
@@ -40,7 +46,7 @@ func (bNumNeutron *bigIntNumNeutron) inverse(
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
 		errPrefDto,
-		"bigIntNumNeutron.Inverse",
+		"bigIntNumNeutron.inverseBigIntNum",
 		"")
 
 	if err != nil {
@@ -93,7 +99,7 @@ func (bNumNeutron *bigIntNumNeutron) inverse(
 		return BigIntNum{},
 			&ReturnBasicError{
 				ErrPrefix: ePrefix.String(),
-				ReturnFunc: "  inverse, err := BigIntMathDivide{}.\n" +
+				ReturnFunc: "inverseBigIntNum, err := BigIntMathDivide{}.\n" +
 					"    BigIntNumFracQuotient(bIOne, bITwo, maxPrecision)",
 				ErrMessage: err.Error(),
 			}
@@ -102,7 +108,7 @@ func (bNumNeutron *bigIntNumNeutron) inverse(
 	return inverse, nil
 }
 
-// isEvenNumber
+// isEvenBigIntNumber
 //
 // Returns true if the current BigIntNum value is evenly
 // divisible by 2.
@@ -120,7 +126,13 @@ func (bNumNeutron *bigIntNumNeutron) inverse(
 // In particular, zero is an even number."
 //
 // https://en.wikipedia.org/wiki/Parity_(mathematics)
-func (bNumNeutron *bigIntNumNeutron) isEvenNumber(
+//
+//	NOTE:
+//
+// This method does NOT test the validity of 'bNum', an
+// instance of type BigIntNum. The calling method must
+// do this!
+func (bNumNeutron *bigIntNumNeutron) isEvenBigIntNumber(
 	bNum *BigIntNum,
 	errPrefDto *ePref.ErrPrefixDto) (bool, error) {
 
@@ -139,7 +151,7 @@ func (bNumNeutron *bigIntNumNeutron) isEvenNumber(
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
 		errPrefDto,
-		"bigIntNumNeutron.Inverse",
+		"bigIntNumNeutron.isEvenBigIntNumber",
 		"")
 
 	if err != nil {
@@ -203,6 +215,12 @@ func (bNumNeutron *bigIntNumNeutron) isEvenNumber(
 // The numeric separators (decimal separator, thousands separator
 // and currency symbol) from the original BigIntNum will remain
 // unchanged.
+//
+//	NOTE:
+//
+// This method does NOT test the validity of 'bNum', an
+// instance of type BigIntNum. The calling method must
+// do this!
 func (bNumNeutron *bigIntNumNeutron) incrementBigIntNum(
 	bNum *BigIntNum,
 	errPrefDto *ePref.ErrPrefixDto) error {
@@ -222,7 +240,7 @@ func (bNumNeutron *bigIntNumNeutron) incrementBigIntNum(
 	ePrefix,
 		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
 		errPrefDto,
-		"bigIntNumNeutron.Inverse",
+		"bigIntNumNeutron.incrementBigIntNum",
 		"")
 
 	if err != nil {
@@ -298,4 +316,220 @@ func (bNumNeutron *bigIntNumNeutron) incrementBigIntNum(
 	}
 
 	return nil
+}
+
+// modBigIntNum
+//
+// Performs a modulo operation where the current BigIntNum numeric value is the
+// dividend and the divisor is the input parameter, 'divisor'.  The modulo operation finds
+// the remainder after division of one number by another (sometimes called modulus).
+// (Wikipedia: https://en.wikipedia.org/wiki/Modulo_operation)
+//
+//		 									dividend = bNum
+//	  									dividend % divisor = modulo
+//
+// The result of this modulo operation is returned as a BigIntNum, 'modulo'. 'modulo' may
+// consist of an integer or a floating point value consisting of integer and fractional
+// digits.
+//
+// Input parameter 'maxPrecision' is used to control the maximum precision of the resulting
+// floating point 'modulo'. Precision is defined as the number of fractional digits to
+// the right of the decimal place. Be advised that these calculations can support very large
+// precision values.
+//
+// The returned BigIntNum instance, 'modulo', will contain numeric separators (decimal
+// separator, thousands separator and currency symbol) copied from the current BigIntNum
+// instance (bNum).
+//
+//	NOTE:
+//
+// This method does NOT test the validity of 'bNum', an
+// instance of type BigIntNum. The calling method must
+// do this!
+//
+// However, this method will vallidate input parameter
+// 'divisor'.
+func (bNumNeutron *bigIntNumNeutron) modBigIntNum(
+	bNum *BigIntNum,
+	divisor *BigIntNum,
+	maxPrecision uint,
+	errPrefDto *ePref.ErrPrefixDto) (modulo BigIntNum, err error) {
+
+	if bNumNeutron.lock == nil {
+		bNumNeutron.lock = new(sync.Mutex)
+	}
+
+	bNumNeutron.lock.Lock()
+
+	defer bNumNeutron.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"bigIntNumNeutron.modBigIntNum",
+		"")
+
+	if err != nil {
+		return modulo, err
+	}
+
+	if bNum == nil {
+
+		return modulo,
+			&ReturnBasicError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "",
+				ErrContext: "",
+				ErrMessage: "FATAL ERROR: Input parameter 'bNum' is a nil pointer.",
+			}
+	}
+
+	if divisor == nil {
+
+		return modulo,
+			&ReturnBasicError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "",
+				ErrContext: "",
+				ErrMessage: "FATAL ERROR: Input parameter 'divisor' is a nil pointer.",
+			}
+	}
+
+	err = new(bigIntNumAtom).isBigIntNumValid(
+		divisor,
+		ePrefix.XCpy(" Validating 'divisor'"))
+
+	if err != nil {
+		return modulo, err
+	}
+
+	biNum2, err := new(bigIntNumUtility).bigIntNumCopyOut(
+		bNum, ePrefix)
+
+	if err != nil {
+
+		return modulo, err
+	}
+
+	modulo, err = BigIntMathDivide{}.BigIntNumModulo(biNum2, *divisor, maxPrecision)
+
+	if err != nil {
+
+		return modulo,
+			&ReturnBasicError{
+				ErrPrefix: ePrefix.String(),
+				ReturnFunc: "modulo, err = BigIntMathDivide{}.BigIntNumModulo(\n" +
+					"    biNum2, *divisor, maxPrecision)",
+				ErrContext: "",
+				ErrMessage: err.Error(),
+			}
+	}
+
+	return modulo, err
+}
+
+// multiplyBigIntNum
+//
+// Multiplies the numerical value of the current BigIntNum instance
+// ('multiplier') times input parameter 'multiplicand'. The 'product' of this
+// multiplication operation is returned as a BigIntNum.
+//
+//	multiplier = bNum
+//	multiplier X multiplicand = product
+//
+// The BigIntNum instance returned by this method, 'product', will contain numeric
+// separators (decimal separator, thousands separator and currency symbol)
+// copied from the current BigIntNum instance.
+//
+//	NOTE:
+//
+// This method does NOT test the validity of 'bNum', an
+// instance of type BigIntNum. The calling method must
+// do this!
+//
+// However, this method will vallidate input parameter
+// 'multiplicand'.
+func (bNumNeutron *bigIntNumNeutron) multiplyBigIntNum(
+	bNum *BigIntNum,
+	multiplicand *BigIntNum,
+	errPrefDto *ePref.ErrPrefixDto) (product BigIntNum, err error) {
+
+	if bNumNeutron.lock == nil {
+		bNumNeutron.lock = new(sync.Mutex)
+	}
+
+	bNumNeutron.lock.Lock()
+
+	defer bNumNeutron.lock.Unlock()
+
+	var ePrefix *ePref.ErrPrefixDto
+
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"bigIntNumNeutron.modBigIntNum",
+		"")
+
+	if err != nil {
+		return BigIntNum{}, err
+	}
+
+	product = new(bigIntNumMechanics).new()
+
+	if bNum == nil {
+
+		return product,
+			&ReturnBasicError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "",
+				ErrContext: "",
+				ErrMessage: "FATAL ERROR: Input parameter 'bNum' is a nil pointer.",
+			}
+	}
+
+	if multiplicand == nil {
+
+		return product,
+			&ReturnBasicError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "",
+				ErrContext: "",
+				ErrMessage: "FATAL ERROR: Input parameter 'multiplicand' is a nil pointer.",
+			}
+	}
+
+	err = new(bigIntNumAtom).isBigIntNumValid(
+		multiplicand,
+		ePrefix.XCpy(" Validating 'multiplicand'"))
+
+	if err != nil {
+		return product, err
+	}
+
+	bNum2, err := new(bigIntNumUtility).bigIntNumCopyOut(
+		bNum,
+		ePrefix)
+
+	if err != nil {
+
+		return product, err
+	}
+
+	product, err = new(BigIntMathMultiply).MultiplyBigIntNums(bNum2, *multiplicand)
+
+	if err != nil {
+
+		return BigIntNum{},
+			&ReturnBasicError{
+				ErrPrefix: ePrefix.String(),
+				ReturnFunc: "product, err = new(BigIntMathMultiply).MultiplyBigIntNums(\n" +
+					"    bNum2, *multiplicand)",
+				ErrContext: "",
+				ErrMessage: err.Error(),
+			}
+	}
+
+	return product, err
 }
