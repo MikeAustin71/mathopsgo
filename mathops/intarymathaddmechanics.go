613f4559ac1689c6f6bede5464a0a823e2bb1e6d
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-07-13 01:08:47 -0500

    Refactored IntAryMathDivide.Divide

--- mathops/intarymathaddmechanics.go
+++ mathops/intarymathaddmechanics.go
@@ -1,12 +1,12 @@
 package mathops
 
 import (
-  ePref "github.com/MikeAustin71/errpref"
-  "sync"
+	ePref "github.com/MikeAustin71/errpref"
+	"sync"
 )
 
 type intAryMathAddMechanics struct {
-  lock sync.Mutex
+	lock sync.Mutex
 }
 
 // RunTotal
@@ -31,235 +31,235 @@ type intAryMathAddMechanics struct {
 //	symbol) copied from the original 'ia' structure. In other
 //	words, the 'ia1' numeric separators will remain unchanged.
 func (iaAddMech *intAryMathAddMechanics) runTotal(
-  ia1 *IntAry,
-  validateIa1 bool,
-  ia2 *IntAry,
-  validateIa2 bool,
-  errPrefDto *ePref.ErrPrefixDto) error {
+	ia1 *IntAry,
+	validateIa1 bool,
+	ia2 *IntAry,
+	validateIa2 bool,
+	errPrefDto *ePref.ErrPrefixDto) error {
 
-  iaAddMech.lock.Lock()
+	iaAddMech.lock.Lock()
 
-  defer iaAddMech.lock.Unlock()
+	defer iaAddMech.lock.Unlock()
 
-  var ePrefix *ePref.ErrPrefixDto
-  var err error
+	var ePrefix *ePref.ErrPrefixDto
+	var err error
 
-  ePrefix,
-    err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
-    errPrefDto,
-    "intAryMathAddMechanics.runTotal()",
-    "")
+	ePrefix,
+		err = ePref.ErrPrefixDto{}.NewFromErrPrefDto(
+		errPrefDto,
+		"intAryMathAddMechanics.runTotal()",
+		"")
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  if ia1 == nil {
+	if ia1 == nil {
 
-    return &InputPtrNilError{
-      ErrPrefix:     ePrefix.String(),
-      ParameterName: "'ia1'",
-    }
-  }
+		return &InputPtrNilError{
+			ErrPrefix:     ePrefix.String(),
+			ParameterName: "'ia1'",
+		}
+	}
 
-  if ia2 == nil {
+	if ia2 == nil {
 
-    return &InputPtrNilError{
-      ErrPrefix:     ePrefix.String(),
-      ParameterName: "'ia2'",
-    }
-  }
+		return &InputPtrNilError{
+			ErrPrefix:     ePrefix.String(),
+			ParameterName: "'ia2'",
+		}
+	}
 
-  if validateIa1 {
+	if validateIa1 {
 
-    err = ia1.IsValid(ePrefix.XCpy("Validating 'ia1'").String())
+		err = ia1.IsValid(ePrefix.XCpy("Validating 'ia1'").String())
 
-    if err != nil {
+		if err != nil {
 
-      return &FuncReturnError{
-        ErrPrefix:  ePrefix.String(),
-        ReturnFunc: "err = ia1.IsValid(ePrefix.XCpy(Validating 'ia1').String())",
-        ErrContext: "Input parameter 'ia1' is INVALID!\n" +
-          "'ia1' FAILED Validatin Tests.",
-        ErrMessage: err.Error(),
-      }
-    }
+			return &FuncReturnError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "err = ia1.IsValid(ePrefix.XCpy(Validating 'ia1').String())",
+				ErrContext: "Input parameter 'ia1' is INVALID!\n" +
+					"'ia1' FAILED Validatin Tests.",
+				ErrMessage: err.Error(),
+			}
+		}
 
-  } else {
+	} else {
 
-    err = ia1.SetInternalFlags()
+		err = ia1.SetInternalFlags()
 
-    if err != nil {
+		if err != nil {
 
-      return &FuncReturnError{
-        ErrPrefix:  ePrefix.String(),
-        ReturnFunc: "err = ia1.SetInternalFlags()",
-        ErrContext: "",
-        ErrMessage: err.Error(),
-      }
-    }
-  }
+			return &FuncReturnError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "err = ia1.SetInternalFlags()",
+				ErrContext: "",
+				ErrMessage: err.Error(),
+			}
+		}
+	}
 
-  if validateIa2 {
+	if validateIa2 {
 
-    err = ia2.IsValid(ePrefix.XCpy("Validating 'ia2'").String())
+		err = ia2.IsValid(ePrefix.XCpy("Validating 'ia2'").String())
 
-    if err != nil {
+		if err != nil {
 
-      return &FuncReturnError{
-        ErrPrefix:  ePrefix.String(),
-        ReturnFunc: "err = ia2.IsValid(ePrefix.XCpy(Validating 'ia2').String())",
-        ErrContext: "Input parameter 'ia2' is INVALID!\n" +
-          "'ia2' FAILED Validatin Tests.",
-        ErrMessage: err.Error(),
-      }
-    }
+			return &FuncReturnError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "err = ia2.IsValid(ePrefix.XCpy(Validating 'ia2').String())",
+				ErrContext: "Input parameter 'ia2' is INVALID!\n" +
+					"'ia2' FAILED Validatin Tests.",
+				ErrMessage: err.Error(),
+			}
+		}
 
-  } else {
+	} else {
 
-    err = ia2.SetInternalFlags()
+		err = ia2.SetInternalFlags()
 
-    if err != nil {
+		if err != nil {
 
-      return &FuncReturnError{
-        ErrPrefix:  ePrefix.String(),
-        ReturnFunc: "err = ia2.SetInternalFlags()",
-        ErrContext: "",
-        ErrMessage: err.Error(),
-      }
-    }
-  }
+			return &FuncReturnError{
+				ErrPrefix:  ePrefix.String(),
+				ReturnFunc: "err = ia2.SetInternalFlags()",
+				ErrContext: "",
+				ErrMessage: err.Error(),
+			}
+		}
+	}
 
-  err = ia1.SetEqualArrayLengths(ia2)
+	err = ia1.SetEqualArrayLengths(ia2)
 
-  if err != nil {
+	if err != nil {
 
-    return &FuncReturnError{
-      ErrPrefix:  ePrefix.String(),
-      ReturnFunc: "err = ia1.SetEqualArrayLengths(ia2)",
-      ErrContext: "",
-      ErrMessage: err.Error(),
-    }
-  }
+		return &FuncReturnError{
+			ErrPrefix:  ePrefix.String(),
+			ReturnFunc: "err = ia1.SetEqualArrayLengths(ia2)",
+			ErrContext: "",
+			ErrMessage: err.Error(),
+		}
+	}
 
-  if ia2.isZeroValue {
-    return nil
-  }
+	if ia2.isZeroValue {
+		return nil
+	}
 
-  ia1NumSeps, err := ia1.GetNumericSeparatorsDto()
+	ia1NumSeps, err := ia1.GetNumericSeparatorsDto()
 
-  if err != nil {
+	if err != nil {
 
-    return &FuncReturnError{
-      ErrPrefix:  ePrefix.String(),
-      ReturnFunc: "ia1NumSeps, err := ia.GetNumericSeparatorsDto()",
-      ErrContext: "",
-      ErrMessage: err.Error(),
-    }
-  }
+		return &FuncReturnError{
+			ErrPrefix:  ePrefix.String(),
+			ReturnFunc: "ia1NumSeps, err := ia.GetNumericSeparatorsDto()",
+			ErrContext: "",
+			ErrMessage: err.Error(),
+		}
+	}
 
-  compare, err := ia1.CompareAbsoluteValues(ia2)
+	compare, err := ia1.CompareAbsoluteValues(ia2)
 
-  if err != nil {
+	if err != nil {
 
-    return &FuncReturnError{
-      ErrPrefix:  ePrefix.String(),
-      ReturnFunc: "compare, err := ia.CompareAbsoluteValues(ia2)",
-      ErrContext: "",
-      ErrMessage: err.Error(),
-    }
-  }
+		return &FuncReturnError{
+			ErrPrefix:  ePrefix.String(),
+			ReturnFunc: "compare, err := ia.CompareAbsoluteValues(ia2)",
+			ErrContext: "",
+			ErrMessage: err.Error(),
+		}
+	}
 
-  newSignVal := ia1.signVal
-  doAdd := true
-  isZeroResult := false
-  doReverseNums := false
+	newSignVal := ia1.signVal
+	doAdd := true
+	isZeroResult := false
+	doReverseNums := false
 
-  if compare == 1 {
-    // compare == + 1
-    // Absolute Value: N1 > N2
+	if compare == 1 {
+		// compare == + 1
+		// Absolute Value: N1 > N2
 
-    if ia1.signVal == 1 && ia2.signVal == 1 {
-      doAdd = true
-      newSignVal = 1
-    } else if ia1.signVal == -1 && ia2.signVal == 1 {
-      doAdd = false
-      newSignVal = -1
-    } else if ia1.signVal == -1 && ia2.signVal == -1 {
-      doAdd = true
-      newSignVal = -1
-    } else {
-      // Must Be ia.signVal == 1 && ia2.signVal == -1
-      doAdd = false
-      newSignVal = 1
-    }
+		if ia1.signVal == 1 && ia2.signVal == 1 {
+			doAdd = true
+			newSignVal = 1
+		} else if ia1.signVal == -1 && ia2.signVal == 1 {
+			doAdd = false
+			newSignVal = -1
+		} else if ia1.signVal == -1 && ia2.signVal == -1 {
+			doAdd = true
+			newSignVal = -1
+		} else {
+			// Must Be ia.signVal == 1 && ia2.signVal == -1
+			doAdd = false
+			newSignVal = 1
+		}
 
-  } else if compare == -1 {
-    // Absolute Values: N2 > N1
-    if ia1.signVal == 1 && ia2.signVal == 1 {
-      doAdd = true
-      newSignVal = 1
-    } else if ia1.signVal == -1 && ia2.signVal == 1 {
-      doAdd = false
-      doReverseNums = true
-      newSignVal = 1
-    } else if ia1.signVal == -1 && ia2.signVal == -1 {
-      doAdd = true
-      newSignVal = -1
-    } else {
-      // Must Be ia.signVal == 1 && ia2.signVal == -1
-      doAdd = false
-      doReverseNums = true
-      newSignVal = -1
-    }
+	} else if compare == -1 {
+		// Absolute Values: N2 > N1
+		if ia1.signVal == 1 && ia2.signVal == 1 {
+			doAdd = true
+			newSignVal = 1
+		} else if ia1.signVal == -1 && ia2.signVal == 1 {
+			doAdd = false
+			doReverseNums = true
+			newSignVal = 1
+		} else if ia1.signVal == -1 && ia2.signVal == -1 {
+			doAdd = true
+			newSignVal = -1
+		} else {
+			// Must Be ia.signVal == 1 && ia2.signVal == -1
+			doAdd = false
+			doReverseNums = true
+			newSignVal = -1
+		}
 
-  } else {
-    // Must be compare == 0
-    // Absolute Values: N1==N2
-    if ia1.signVal == 1 && ia2.signVal == 1 {
-      doAdd = true
-      newSignVal = 1
-    } else if ia1.signVal == -1 && ia2.signVal == 1 {
-      doAdd = false
-      newSignVal = 1
-      isZeroResult = true
-    } else if ia1.signVal == -1 && ia2.signVal == -1 {
-      doAdd = true
-      newSignVal = -1
-    } else {
-      // Must Be ia.signVal == 1 && ia2.signVal == -1
-      doAdd = false
-      newSignVal = 1
-      isZeroResult = true
-    }
+	} else {
+		// Must be compare == 0
+		// Absolute Values: N1==N2
+		if ia1.signVal == 1 && ia2.signVal == 1 {
+			doAdd = true
+			newSignVal = 1
+		} else if ia1.signVal == -1 && ia2.signVal == 1 {
+			doAdd = false
+			newSignVal = 1
+			isZeroResult = true
+		} else if ia1.signVal == -1 && ia2.signVal == -1 {
+			doAdd = true
+			newSignVal = -1
+		} else {
+			// Must Be ia.signVal == 1 && ia2.signVal == -1
+			doAdd = false
+			newSignVal = 1
+			isZeroResult = true
+		}
 
-  }
+	}
 
-  err = new(intAryMathAddUtility).addToSubtract(
-    ia1, false, ia2, false, newSignVal, doAdd, isZeroResult, doReverseNums, ePrefix)
+	err = new(intAryMathAddUtility).addToSubtract(
+		ia1, false, ia2, false, newSignVal, doAdd, isZeroResult, doReverseNums, ePrefix)
 
-  if err != nil {
+	if err != nil {
 
-    return &FuncReturnError{
-      ErrPrefix: ePrefix.String(),
-      ReturnFunc: "err = new(intAryMathAddUtility).addToSubtract(\n" +
-        "ia, ia2, newSignVal, doAdd, isZeroResult, doReverseNums, ePrefix)",
-      ErrContext: "",
-      ErrMessage: err.Error(),
-    }
-  }
+		return &FuncReturnError{
+			ErrPrefix: ePrefix.String(),
+			ReturnFunc: "err = new(intAryMathAddUtility).addToSubtract(\n" +
+				"ia, ia2, newSignVal, doAdd, isZeroResult, doReverseNums, ePrefix)",
+			ErrContext: "",
+			ErrMessage: err.Error(),
+		}
+	}
 
-  err = ia2.SetNumericSeparatorsDto(ia1NumSeps)
+	err = ia2.SetNumericSeparatorsDto(ia1NumSeps)
 
-  if err != nil {
+	if err != nil {
 
-    return &FuncReturnError{
-      ErrPrefix:  ePrefix.String(),
-      ReturnFunc: "err = ia.SetNumericSeparatorsDto(numSeps)",
-      ErrContext: "",
-      ErrMessage: err.Error(),
-    }
-  }
+		return &FuncReturnError{
+			ErrPrefix:  ePrefix.String(),
+			ReturnFunc: "err = ia.SetNumericSeparatorsDto(numSeps)",
+			ErrContext: "",
+			ErrMessage: err.Error(),
+		}
+	}
 
-  return nil
+	return nil
 }
