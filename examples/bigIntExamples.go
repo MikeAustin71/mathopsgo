10f3f5a55b1448602eef8de50093828da6783ca1
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2026-01-02 23:34:42 -0600

    Corrected all errors in bigIntExamples.go

--- examples/bigIntExamples.go
+++ examples/bigIntExamples.go
@@ -2,77 +2,221 @@ package examples
 
 import (
 	"fmt"
-	"github.com/mikeaustin71/mathops"
 	"math/big"
+
+	"github.com/mikeaustin71/mathops"
 )
 
-func ExampleBigIntMagnitude_01(target *big.Int) {
+func ExampleBigIntMagnitude01(target *big.Int) {
 
-	magnitude, err := mathops.BigIntMath{}.GetMagnitude(target)
+	ePrefix := "bigIntExamples.ExampleBigIntMagnitude01"
+
+	magnitude, err := new(mathops.BigIntMath).GetMagnitude(target)
 
 	if err != nil {
-		fmt.Printf("Error returned by BigIntMath{}.GetMagnitudeDigits(target). "+
-			"target='%v' Error='%v' ", target.Text(10), err.Error())
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"magnitude, err := new(mathops.BigIntMath).\n"+
+			" GetMagnitude(target)\n"+
+			"target= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			target.Text(10),
+			err.Error())
 		return
 	}
 
 	bigTen := big.NewInt(10)
+
 	val := big.NewInt(0).Exp(bigTen, magnitude, nil)
 
 	fmt.Println("   target= ", target.Text(10))
 	fmt.Println("magnitude= ", magnitude.Text(10))
 	fmt.Println("ten Value= ", val.Text(10))
 
-	bigTarget := mathops.BigIntNum{}.NewBigInt(target, 0)
-	bigMagnitude := mathops.BigIntNum{}.NewBigInt(magnitude, 0)
-	bigTenValue := mathops.BigIntNum{}.NewBigInt(val, 0)
+	bigTarget, err := new(mathops.BigIntNum).NewBigInt(target, 0)
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bigTarget, err := new(mathops.BigIntNum).\n"+
+			"  NewBigInt(target, 0)\n"+
+			"target= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			target.Text(10),
+			err.Error())
+		return
+	}
+
+	bigMagnitude, err := new(mathops.BigIntNum).NewBigInt(magnitude, 0)
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bigMagnitude, err := new(mathops.BigIntNum).\n"+
+			"  NewBigInt(magnitude, 0)\n"+
+			"magnitude= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			magnitude.Text(10),
+			err.Error())
+		return
+	}
+
+	bigTenValue, err := new(mathops.BigIntNum).NewBigInt(val, 0)
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bigTenValue, err := new(mathops.BigIntNum).\n"+
+			"  NewBigInt(val, 0)\n"+
+			"val= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			val.Text(10),
+			err.Error())
+		return
+	}
+
+	bigTargetFmtThousandsStr, err := bigTarget.FormatThousandsStr(mathops.LEADMINUSNEGVALFMTMODE)
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bigTargetFmtThousandsStr, err := bigTarget.\n"+
+			" FormatThousandsStr(mathops.LEADMINUSNEGVALFMTMODE)\n"+
+			"mathops.LEADMINUSNEGVALFMTMODE= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			mathops.LEADMINUSNEGVALFMTMODE.String(),
+			err.Error())
+		return
+	}
+
+	bigTenValueFmtThousandsStr, err := bigTenValue.FormatThousandsStr(mathops.LEADMINUSNEGVALFMTMODE)
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bigTenValueFmtThousandsStr, err := bigTenValue.\n"+
+			" FormatThousandsStr(mathops.LEADMINUSNEGVALFMTMODE)\n"+
+			"mathops.LEADMINUSNEGVALFMTMODE= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			mathops.LEADMINUSNEGVALFMTMODE.String(),
+			err.Error())
+		return
+	}
+
+	bigMagnitudeNumStr, err := bigMagnitude.GetNumStr()
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bigMagnitudeNumStr, err := bigMagnitude.GetNumStr()\n"+
+			"Error='%v'\n\n", ePrefix, err.Error())
+		return
+	}
+
 	fmt.Println("========================================")
-	fmt.Println("   Target Str: ", bigTarget.FormatThousandsStr(mathops.LEADMINUSNEGVALFMTMODE))
-	fmt.Println("ten Value Str: ", bigTenValue.FormatThousandsStr(mathops.LEADMINUSNEGVALFMTMODE))
-	fmt.Println("    Magnitude: ", bigMagnitude.GetNumStr())
+	fmt.Println("   Target Str: ", bigTargetFmtThousandsStr)
+	fmt.Println("ten Value Str: ", bigTenValueFmtThousandsStr)
+	fmt.Println("    Magnitude: ", bigMagnitudeNumStr)
 
 }
 
-func ExampleBigIntExpTest_01(baseStr, exponentStr, expectedStr string) {
+func ExampleBigIntExpTest01(baseStr, exponentStr, expectedStr string) {
 
-	bINumBase, err := mathops.BigIntNum{}.NewNumStr(baseStr)
+	ePrefix := "bigIntExamples.ExampleBigIntExpTest01"
+
+	bINumBase, err := new(mathops.BigIntNum).NewNumStr(baseStr)
 
 	if err != nil {
-		fmt.Printf("Error returned by BigIntNum{}.NewNumStr(baseStr). "+
-			"baseStr='%v' Error='%v' \n", baseStr, err.Error())
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bINumBase, err := new(mathops.BigIntNum).\n"+
+			" NewNumStr(baseStr)\n"+
+			"baseStr= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			baseStr,
+			err.Error())
 		return
 	}
 
-	bIBase, err := bINumBase.GetBigInt()
+	bINumBaseNumStr, err := bINumBase.GetNumStr()
 
 	if err != nil {
-		fmt.Printf("Error returned by bINumBase.GetBigInt(). "+
-			"bINumBase='%v' Error='%v' \n", bINumBase.GetNumStr(), err.Error())
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bINumBaseNumStr, err := bINumBase.GetNumStr()\n"+
+			"Error='%v'\n\n",
+			ePrefix, err.Error())
 		return
 	}
 
-	bINumExponent, err := mathops.BigIntNum{}.NewNumStr(exponentStr)
+	bIBaseBigInt, err := bINumBase.GetBigInt()
 
 	if err != nil {
-		fmt.Printf("Error returned by BigIntNum{}.NewNumStr(exponentStr). "+
-			"exponentStr='%v' Error='%v' \n", exponentStr, err.Error())
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bIBaseBigInt, err := bINumBase.GetBigInt()\n"+
+			"bINumBase= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			bINumBaseNumStr,
+			err.Error())
 		return
 	}
 
-	bIExponent, err := bINumExponent.GetBigInt()
+	bINumExponent, err := new(mathops.BigIntNum).NewNumStr(exponentStr)
 
 	if err != nil {
-		fmt.Printf("Error returned by bINumExponent.GetBigInt(). "+
-			"bINumExponent='%v' Error='%v' \n", bINumExponent.GetNumStr(), err.Error())
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bINumExponent, err := new(mathops.BigIntNum).\n"+
+			" NewNumStr(exponentStr)\n"+
+			"exponentStr= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			exponentStr,
+			err.Error())
 		return
 	}
+
+	bINumExponentNumStr, err := bINumExponent.GetNumStr()
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bIExponentNumStr, err :=  bINumExponent.GetNumStr()\n"+
+			"Error='%v'\n\n",
+			ePrefix, err.Error())
+		return
+	}
+
+	bIExponentBigInt, err := bINumExponent.GetBigInt()
+
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"bIExponentBigInt, err := bINumExponent.GetBigInt()\n"+
+			"bINumExponent= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			bINumExponentNumStr,
+			err.Error())
+		return
+	}
+
 	modM := big.NewInt(0)
-	result := big.NewInt(0).Exp(bIBase, bIExponent, modM)
+	result := big.NewInt(0).Exp(bIBaseBigInt, bIExponentBigInt, modM)
 
 	fmt.Println("big.NewInt(0).Exp(...)")
 	fmt.Println("-------------------------------")
-	fmt.Println("    Base: ", bIBase.Text(10))
-	fmt.Println("Exponent: ", bIExponent.Text(10))
+	fmt.Println("    Base: ", bIBaseBigInt.Text(10))
+	fmt.Println("Exponent: ", bIExponentBigInt.Text(10))
 	fmt.Println("-------------------------------")
 	fmt.Println("Expected: ", expectedStr)
 	fmt.Println("  Result: ", result.Text(10))
