dc79a8a2dc1e3ce44c1bd1971caaa594e62947f1
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2026-01-04 02:19:16 -0600

    Corrected errors through TestFixDecNthRootGetNextFracBundle; examples/maintests.go

--- examples/decimalexamples.go
+++ examples/decimalexamples.go
@@ -1,73 +1,73 @@
 package examples
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/mikeaustin71/mathops"
+	"github.com/mikeaustin71/mathops"
 )
 
 func ExampleDecPowInt01(baseStr string, exponent int, maxPrecision uint, expectedStr string) {
 
-  ePrefix := "ExampleDecPowInt01"
+	ePrefix := "ExampleDecPowInt01"
 
-  d1, err := new(mathops.Decimal).NewNumStr(baseStr)
+	d1, err := new(mathops.Decimal).NewNumStr(baseStr)
 
-  if err != nil {
-    fmt.Printf("%v\n"+
-      "Error returned by:\n"+
-      "d1, err := new(mathops.Decimal).NewNumStr(baseStr)\n"+
-      "baseStr= '%v'\n"+
-      "Error='%v'\n\n",
-      ePrefix,
-      baseStr,
-      err.Error())
-    return
-  }
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"d1, err := new(mathops.Decimal).NewNumStr(baseStr)\n"+
+			"baseStr= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			baseStr,
+			err.Error())
+		return
+	}
 
-  d2, err := d1.PowInt(exponent, maxPrecision)
+	d2, err := d1.PowInt(exponent, maxPrecision)
 
-  if err != nil {
-    fmt.Printf("%v\n"+
-      "Error returned by:\n"+
-      "d2, err := d1.PowInt(exponent, maxPrecision)\n"+
-      "exponent= '%v'\n"+
-      "maxPrecision= '%v'\n"+
-      "Error='%v'\n\n",
-      ePrefix,
-      exponent,
-      maxPrecision,
-      err.Error())
-    return
-  }
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"d2, err := d1.PowInt(exponent, maxPrecision)\n"+
+			"exponent= '%v'\n"+
+			"maxPrecision= '%v'\n"+
+			"Error='%v'\n\n",
+			ePrefix,
+			exponent,
+			maxPrecision,
+			err.Error())
+		return
+	}
 
-  actualResult, err := d2.GetNumStr()
+	actualResult, err := d2.GetNumStr()
 
-  if err != nil {
-    fmt.Printf("%v\n"+
-      "Error returned by:\n"+
-      "actualResult, err := d2.GetNumStr()\n"+
-      "Error='%v'\n\n",
-      ePrefix, err.Error())
-    return
-  }
+	if err != nil {
+		fmt.Printf("%v\n"+
+			"Error returned by:\n"+
+			"actualResult, err := d2.GetNumStr()\n"+
+			"Error='%v'\n\n",
+			ePrefix, err.Error())
+		return
+	}
 
-  fmt.Println("   Decimal.PowInt()")
-  fmt.Println("------------------------")
+	fmt.Println("   Decimal.PowInt()")
+	fmt.Println("------------------------")
 
-  if expectedStr == actualResult {
+	if expectedStr == actualResult {
 
-    fmt.Println("*** SUCCESS ***")
+		fmt.Println("*** SUCCESS ***")
 
-  } else {
+	} else {
 
-    fmt.Println("@@@ FAILURE @@@")
+		fmt.Println("@@@ FAILURE @@@")
 
-  }
+	}
 
-  fmt.Println("------------------------")
-  fmt.Println("           Base: ", baseStr)
-  fmt.Println("       Exponent: ", exponent)
-  fmt.Println("   maxPrecision: ", maxPrecision)
-  fmt.Println("  Actual Result: ", actualResult)
-  fmt.Println("Expected Result: ", expectedStr)
+	fmt.Println("------------------------")
+	fmt.Println("           Base: ", baseStr)
+	fmt.Println("       Exponent: ", exponent)
+	fmt.Println("   maxPrecision: ", maxPrecision)
+	fmt.Println("  Actual Result: ", actualResult)
+	fmt.Println("Expected Result: ", expectedStr)
 }
